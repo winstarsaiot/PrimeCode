95a0658c6e6b707ea74bb2d1da8285ada0515b1f
winstarsaiot <92361230+winstarsaiot@users.noreply.github.com>
2021-10-15 15:02:56 +0800

    Update 11-4_umbrellabot.py

--- 11-4_umbrellabot.py
+++ 11-4_umbrellabot.py
@@ -3,14 +3,15 @@ from pyatcrobo2.parts import Servomotor, UltrasonicSensor
 from pystubit.board import display, Image
 import time, json
 
-# Servomotor and Ultrasonic Sensor settings
+sv13 = Servomotor('P13')
+sv14 = Servomotor('P14')
+us = UltrasonicSensor("P0")
 
-#WIFI settings
-WIFI_SSID = ""
-WIFI_PWD = ""
+WIFI_SSID = "WINSTARS_INTERN_2"
+WIFI_PWD = "winstars1234"
 response = wifi_connect(WIFI_SSID, WIFI_PWD, trytime=3)
 
-CITY_ID = 1 # Hong Kong City ID
+CITY_ID = 1
 
 WORDS_FINE = {"No Rain", "Fine", "Sunny", "Clear", "Clearing", "Bright",
                 "Fair", "Mild"}
@@ -30,11 +31,26 @@ IMG_CLOUDY = Image("00000:01110:11111:01110:00000:", color=(15, 15, 15))
 IMG_SNOWY = Image("10101:01110:11011:01110:10101:", color=(30, 30, 30))
 IMG_OTHER = Image("00000:00000:01110:00000:00000:", color=(10, 5, 15))
 
+# User define
 def give_umbrella():
-    # ...
+    sv13.set_angle(45)
+    sv14.set_angle(135)
+    time.sleep(3)
+    sv13.set_angle(90)
+    sv14.set_angle(90)
 
 def get_icon(weather):
-    # ...
+    if weather in WORDS_FINE:
+        img = IMG_FINE
+    elif weather in WORDS_RAINY:
+        img = IMG_RAINY
+    elif weather in WORDS_CLOUDY:
+        img = IMG_CLOUDY
+    elif weather in WORDS_SNOWY:
+        img = IMG_SNOWY
+    else:
+        img = IMG_OTHER
+    return img
 
 def get_forecast():
     url_format = "https://worldweather.wmo.int/en/json/[City ID]_en.json"
@@ -51,3 +67,9 @@ def get_forecast():
         give_umbrella()
 
 # main program
+sv13.set_angle(90)
+sv14.set_angle(90)
+while True:
+    d = us.get_distance()
+    if d > 0 and d < 30:
+        get_forecast()
