7a8bbfed660ea31387aa5afa666bfaae2f589a2e
winstarsaiot <92361230+winstarsaiot@users.noreply.github.com>
2021-10-15 14:48:06 +0800

    Create 4-2_Anti-Collision Car.py

---
+++ 4-2_Anti-Collision Car.py
@@ -0,0 +1,37 @@
+from pystubit.board import button_a, buzzer
+from pyatcrobo2.parts import DCMotor, UltrasonicSensor
+import time
+
+def alarm():
+    for i in range(3):
+        buzzer.on('C7',duration=50)
+        time.sleep_ms(50)
+
+us = UltrasonicSensor('P0')
+dcm = DCMotor('M1')
+dcm.power(100)
+
+while True:
+    if button_a.is_pressed():
+        distance = us.get_distance()
+        if distance > 0 and distance < 10:
+            alarm()
+        if distance < 5:
+            dcm.brake()
+        else:
+            dcm.cw()
+    else:  
+        dcm.stop()
+
+'''
+# An Anti-Fall Robocar
+from pyatcrobo2.parts import DCMotor, UltrasonicSensor
+
+us = UltrasonicSensor('P0')
+dcm = DCMotor('M1')
+dcm.power(100)
+
+while True:
+    distance = us.get_distance()
+
+'''
